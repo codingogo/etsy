f3bf98b4e191aac5271f517148588435b336e905
ji <gafrong@gmail.com>
2016-07-04 19:41:20 +1000

    carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
     :provider => 'AWS',
     :aws_access_key_id => ENV['aws_access_key_id'],
     :aws_secret_access_key => ENV['aws_secret_access_key'],
+    :endpoint => "https://s3.amazonaws.com"
   }
   config.fog_directory = ENV['fog_directory']
 end
