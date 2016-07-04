04632872c88bdf019327b359ae37fc2dbbd59915
ji <gafrong@gmail.com>
2016-07-04 22:43:24 +1000

    region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider => 'AWS',
-    :aws_access_key_id => ENV['aws_access_key_id'],
-    :aws_secret_access_key => ENV['aws_secret_access_key'],
-    :region  => 'seoul'
+    :provider 								=> 'AWS',
+    :aws_access_key_id 				=> ENV['aws_access_key_id'],
+    :aws_secret_access_key 		=> ENV['aws_secret_access_key'],
+    :region  									=> 'ap-northeast-2'
   }
   config.fog_directory = 'etsyapplication'
 end
